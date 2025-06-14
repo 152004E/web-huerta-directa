93709e175953ffb325875f3d4359f4ca693ba392
Santiago <santipuentes2017@gmial.com>
2025-06-14 11:07:00 -0500

    Actulizando controlador

--- controllers/controlador.php
+++ controllers/controlador.php
@@ -1,4 +1,31 @@
 <?php
 
+//var_dump($_POST);
 
+include "../models/usuario.php";
+
+$usuario = new usuarios();
+$respuesta = $usuario->Login($_POST["email"], $_POST["password_user"]);
+
+//var_dump($respuesta);
+
+if($respuesta instanceof Exception){
+    header("location:../views/Errores/error500.html");
+}
+else if(!empty($respuesta)){
+    if($respuesta[0]["perfil_usuario"]=="1"){
+        header("location:../views/administrador.html");
+    }
+    else if ($respuesta[0]["perfil_usuario"]=="2"){
+        header("location:../views/coordinador.html");
+    }
+}
+else {
+    echo "
+        <script>
+            alert('Datos incorrectos, vuelva a intentar');
+            location.href='../views/login-20250405T183650Z-001/pagina.html';
+        </script>
+    ";
+}
 ?>
