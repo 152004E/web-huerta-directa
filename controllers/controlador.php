faacaf4c0fd55d3ad8ebfe873c1d35a3027d4819
Santiago <santipuentes2017@gmail.com>
2025-06-16 20:41:53 -0500

    update controllers

--- controllers/controlador.php
+++ controllers/controlador.php
@@ -13,18 +13,18 @@ if($respuesta instanceof Exception){
     header("location:../views/Errores/error500.html");
 }
 else if(!empty($respuesta)){
-    if($respuesta[0]["fk_id"]=="1"){
+    if($respuesta[0]["fk_id_role"]=="1"){
         header("location:../views/Dashboard/dashboard.html");
     }
-    else if ($respuesta[0]["fk_id"]=="2"){
-        header("location:../views/coordinador.html");
+    else if ($respuesta[0]["fk_id_role"]=="2"){
+        header("location:../views/index.html");
     }
 }
 else {
     echo "
         <script>
             alert('Datos incorrectos, vuelva a intentar');
-            location.href='../views/login-20250405T183650Z-001/pagina.html';
+            location.href='../views/login/pagina.html';
         </script>
     ";
 }
