32f4bc930023038a99fef100692511ec1fd818fd
jesus.huerta <jjpp142007@gmail.com>
2025-07-04 23:45:30 -0500

    changes for error

--- controllers/Administrador/Registro_admin.php
+++ controllers/Administrador/Registro_admin.php
@@ -1,43 +1,15 @@
 <?php
-// Incluye el modelo de administradores
 include "../../models/administradores.php";
 
-<<<<<<< HEAD
 // Verifica si los datos llegaron correctamente por POST
 if (
     isset($_POST["Nombre_usuario"]) &&
     isset($_POST["email"]) &&
     isset($_POST["password"])
 ) {
-    // Crear instancia del modelo
+
     $admin = new administradores();
-=======
 
-$usuario = new usuarios();
-
-//$respuesta = $usuario->RegistrarAdmin( $_POST["nombre_usuario"], $_POST["password"] );
-
-
-if ($respuesta === true) {
-    echo '
-    <script>
-        alert("Usuario actualizado correctamente.");
-        location.href = "../../views/Consulta_usuarios/Consulta_usuario.php";
-    </script>';
-} else {
-    echo '
-    <script>
-        alert("No se pudo actualizar el usuario. Intente nuevamente.");
-        location.href = "../../views/Consulta_usuarios/Consulta_usuario.php";
-    </script>';
-}
-?>
-
-// Crear instancia del modelo
-$admin = new administradores();
->>>>>>> 21a20c89303cea57b840132cfc0875405d2bf02d
-
-    // Capturar los datos del formulario
     $nombre = $_POST["Nombre_usuario"];
     $correo = $_POST["email"];
     $clave = $_POST["password"];
@@ -56,16 +28,16 @@ $admin = new administradores();
         echo "
             <script>
                 alert('No se pudo registrar el administrador. Intente nuevamente.');
-                location.href='../../views/dashboard/Dashboardd.php';
+                location.href='../../../views/dashboard/Dashboardd.php';
             </script>
         ";
     }
 } else {
-    // Si no llegan todos los datos esperados
+    // Jaja no llegan a llegar datos
     echo "
         <script>
             alert('Faltan datos en el formulario.');
-            location.href='../../views/dashboard/Dashboardd.php';
+            location.href='../../../views/dashboard/Dashboardd.php';
         </script>
     ";
 }
