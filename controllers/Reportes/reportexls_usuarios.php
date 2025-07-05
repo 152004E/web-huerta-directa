cb708d6a96d1a5bf4f8680dd37107fe59b909cbd
Yeferson Sanchez <jefersonsnachez@gmail.com>
2025-07-04 23:24:33 -0500

    Yeferson: updating reporte excel

--- controllers/Reportes/reportexls_usuarios.php
+++ controllers/Reportes/reportexls_usuarios.php
@@ -2,16 +2,19 @@
 //El contenido será Excel y acepta caracteres especiales
 header("Content-Type: application/vnd.ms-excel; charset=ISO-8859-1");
 //Se comporta descargando un archivo y lo nombre usuarios.xls
-header("Content-Disposition: attachment; filename=usuarios.xls");
+header("Content-Disposition: attachment; filename=reporte_usuarios.xls");
 header("Pragma: no-cache");
 header("Expires: 0");
 
 ob_start();
 
 //Carga los datos de la ruta específica, para este caso desde los datos de sesión
-session_start();
-$respuesta = $_SESSION["respuesta"];
+//session_start();
+//$respuesta = $_SESSION["respuesta"];
 
+include "../../models/usuarios.php";
+$usuario = new usuarios();
+$respuesta = $usuario->ConsultaGeneral();
 //Crea una tabla, el Excel asumirá esta disposición
 //Los datos pueden asumir caracteres especiales
 echo "<meta http-equiv='Content-Type' content='text/html; charset=ISO-8859-1'>";
